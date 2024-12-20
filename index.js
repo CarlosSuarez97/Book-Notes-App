3b0c643c023e9a7ace40d0be6c86abc498d3a962
Carlos Suarez <carlos-suarez-97@hotmail.com>
2024-12-19 21:27:37 -0500

    Work on pages layouts and on how the information will be pased from the back to front once the database's ready

--- index.js
+++ index.js
@@ -21,11 +21,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(morgan("tiny"));
 
+let readBooks = [];
+
+//Hard-coded data just to try the layout;
+const readBooksDemo = [{
+    title: "This is a Demo Title",
+    author: "John Doe",
+    dateRead: "2024",
+    rating: "5 out of 5",
+    cover: "This is a placeholder for the cover",
+    plot: "This is the plot",
+    review: "This is a short review"
+}, {
+    title: "This is a Demo Title 2",
+    author: "John Doe 2",
+    dateRead: "2024",
+    rating: "5 out of 5",
+    cover: "This is a placeholder for the cover",
+    plot: "This is the plot",
+    review: "This is a short review"
+}];
+
 //Rendering the welcome page
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {
+        readBooksDemo: readBooksDemo,
+    });
 })
 
+//Page to write a new review
+app.get("/writeReview", async (req, res) => {
+    res.render("writeReview.ejs");
+})
 
 //Designated port for the application
 app.listen(port, () => {
