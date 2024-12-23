aeedd9cc75903d695c222486ccb6da0aa2c33eca
Carlos Suarez <carlos-suarez-97@hotmail.com>
2024-12-22 22:23:08 -0500

    Fix database functionality fetch book covers

--- index.js
+++ index.js
@@ -23,35 +23,44 @@ app.use(morgan("tiny"));
 
 let readBooks = [];
 
-//Hard-coded data just to try the layout;
-const readBooksDemo = [{
-    title: "This is a Demo Title",
-    author: "John Doe",
-    dateRead: "2024",
-    rating: "5 out of 5",
-    cover: "This is a placeholder for the cover",
-    plot: "This is the plot",
-    review: "This is a short review"
-}, {
-    title: "This is a Demo Title 2",
-    author: "John Doe 2",
-    dateRead: "2024",
-    rating: "5 out of 5",
-    cover: "This is a placeholder for the cover",
-    plot: "This is the plot",
-    review: "This is a short review"
-}];
 
 //Rendering the welcome page
 app.get("/", async (req, res) => {
-    res.render("index.ejs", {
-        readBooksDemo: readBooksDemo,
-    });
+    try {
+        const result  = await db.query("SELECT book.id, book.title, book.rating, book.cover, book.plot, book.review, book_details.author, book_details.date_read FROM book INNER JOIN book_details ON book.id = book_details.book_id;");
+        readBooks = result.rows;
+        res.render("index.ejs", {
+            readBooks: readBooks,
+        })
+    } catch(err) {
+        console.log(err);
+    }
 })
 
 //Page to write a new review
 app.get("/writeReview", async (req, res) => {
     res.render("writeReview.ejs");
+})
+
+//Adding a new review to the database
+app.post("/addReview", async (req, res) => {
+    const author = req.body.author;
+    const title = req.body.title;
+    const isbn = req.body.isbn;
+    const plot = req.body.plot;
+    const review = req.body.review;
+    const dateRead = req.body.dateRead;
+    const rating = req.body.rating;
+    const cover = "https://covers.openlibrary.org/b/isbn/" + isbn + "-M.json";
+
+
+    try {
+        await db.query("INSERT INTO book (title, cover, rating, plot, review, id) VALUES ($1, $2, $3, $4, $5, $6)", [title, cover, rating, plot, review, isbn]).then(db.query("INSERT INTO book_details (author, date_read, book_id) VALUES ($1, $2, $3)", [author, dateRead, isbn]));
+    } catch(err) {
+        console.log(err);
+    }
+
+    res.redirect("/");
 })
 
 //Designated port for the application
